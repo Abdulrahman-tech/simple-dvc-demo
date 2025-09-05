cc44c4dc3c76436dceaabe715d35d27a981da167
Abdulrahman-tech <abdullateefa01@gmail.com>
2025-09-05 11:54:01 +0100

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages # type: ignore
+
+setup(
+    name= "src" ,
+    version= "0.0.1",
+    description="its a wine Q package", 
+    author= "Abdulrahman", 
+    packages=find_packages(), 
+    license="MIT"
+)
+
+
