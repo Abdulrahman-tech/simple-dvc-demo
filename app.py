5664e736ff48459fa3db2e6c51e9358e87c1ef8f
Abdulrahman-tech <abdullateefa01@gmail.com>
2025-09-09 21:54:46 +0100

    github action workflow added

--- app.py
+++ app.py
@@ -27,8 +27,16 @@ def predict(data):
     return prediction[0]
 
 def api_response(request):
-    pass
-
+        try:
+          data =np.array([list(request.json.values())])
+          response = predict(data)
+          response = {"response":response}
+          return response
+        
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again"}
+            return error
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
